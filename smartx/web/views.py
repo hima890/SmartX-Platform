435806fd743605388f0e9dc6317fc2419495caa1
hima890 <hfibrahim90@gmail.com>
2024-12-25 10:05:57 +0200

    add view function for web app

--- smartx/web/views.py
+++ smartx/web/views.py
@@ -1,3 +1,11 @@
+""" This file contains the views for the landing page of the website. """
 from django.shortcuts import render
 
 # Create your views here.
+def landing_page(request):
+    """
+    This view renders the landing page of the website.
+    request: The request object used to generate this page.
+    return: The rendered landing page.
+    """
+    return render(request, 'landing.html')
