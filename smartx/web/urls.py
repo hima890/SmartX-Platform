d48fbd079e439f41c2c3373d6666392787c83c18
hima890 <hfibrahim90@gmail.com>
2024-12-27 08:37:37 +0200

    Fix bug signup/signin-template

--- smartx/web/urls.py
+++ smartx/web/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     # Landing Page
     path('', views.landing_page, name='landing_page'),
+    # Sign In / Sign Up Page
+    path('signin/', views.signin_signup, name='signin_signup'),
 ]
--- smartx/web/views.py
+++ smartx/web/views.py
@@ -1,6 +1,7 @@
 """ This file contains the views for the landing page of the website. """
 from django.shortcuts import render
 
+
 # Create your views here.
 def landing_page(request):
     """
@@ -9,3 +10,12 @@ def landing_page(request):
     return: The rendered landing page.
     """
     return render(request, 'index.html')
+
+
+def signin_signup(request):
+    """
+    This view renders the sign in / sign up page of the website.
+    request: The request object used to generate this page.
+    return: The rendered sign in / sign up page.
+    """
+    return render(request, 'forms.html')
